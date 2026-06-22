36fce56b42071d86fe1629e09f9d2b041aea2873
25051121g <1146877017@qq.com>
2026-06-22 15:59:05 +0800

    Left_test_only2per_v3 basically succeed

--- controlnet_COCO_test_autoprompt/download_model.py
+++
@@ -1,6 +0,0 @@
-import torch
-from diffusers import StableDiffusionPipeline
-
-# 把整个模型安全地下载到系统的默认缓存目录中
-pipe = StableDiffusionPipeline.from_pretrained("Lykon/AbsoluteReality", torch_dtype=torch.float16)
-print("模型下载完成！")
