62aea526449a293c763d01dd3853b6d923e4a818
25051121g <1146877017@qq.com>
2026-07-03 18:25:36 +0800

    finish submit, adjust

--- Left_test_only2per/tmp_test.py
+++
@@ -1,9 +0,0 @@
-import torch
-print("torch version:", torch.__version__)
-print("has float8_e8m0fnu:", hasattr(torch, 'float8_e8m0fnu'))
-
-import transformers
-print("transformers version:", transformers.__version__)
-
-import diffusers
-print("diffusers version:", diffusers.__version__)
